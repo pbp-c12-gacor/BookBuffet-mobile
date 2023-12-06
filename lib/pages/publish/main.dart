fa42ae00dae3a2e155f50eb1003c62823ecdfc70
odracheer <ricardo.palungguk@ui.ac.id>
2023-12-06 16:45:07 +0700

    Commiting to onliine rep

--- lib/pages/publish/main.dart
+++ lib/pages/publish/main.dart
@@ -1,11 +1,23 @@
 import 'package:bookbuffet/main.dart';
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
+import 'dart:convert';
 
-class PublishPage extends StatelessWidget {
+class PublishPage extends StatefulWidget {
   const PublishPage({super.key});
 
   @override
+  _PublishPageState createState() => _PublishPageState();
+}
+
+class _PublishPageState extends State<PublishPage> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Book Buffet'),
