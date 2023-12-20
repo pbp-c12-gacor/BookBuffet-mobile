63bdf992c5d91f834ac1e3e38b511615d8c09b0b
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-12-20 22:17:51 +0700

    Remove unused imports in search.dart and search_book_card.dart

--- lib/pages/catalog/widgets/search_book_card.dart
+++ lib/pages/catalog/widgets/search_book_card.dart
@@ -1,4 +1,3 @@
-import 'package:bookbuffet/main.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
