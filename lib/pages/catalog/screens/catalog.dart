6cc0f26c3fd06a907b9f54171e0f392c6abd0242
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-12-20 10:26:11 +0700

    Add books by category functionality and check for new books

--- lib/pages/catalog/screens/catalog.dart
+++ lib/pages/catalog/screens/catalog.dart
@@ -17,16 +17,19 @@ class Catalog extends StatefulWidget {
 class _CatalogState extends State<Catalog> {
   late Future<List<Book>> _books = ApiService.getBooks();
   late Future<List<Category>> _categories = ApiService.getCategories();
+  late Future<List<Future<List<Book>>>> _booksByCategory = Future.value([]);
   int _length = 0;
 
   @override
   void initState() {
     super.initState();
     loadData();
+    checkForNewBooks();
   }
 
   void loadData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    //await prefs.clear();
     if (prefs.getString('books') != null) {
       _books = Future.value(List<Book>.from(
           jsonDecode(prefs.getString('books')!).map((x) => Book.fromJson(x))));
@@ -35,6 +38,28 @@ class _CatalogState extends State<Catalog> {
       _categories = Future.value(List<Category>.from(
           jsonDecode(prefs.getString('categories')!)
               .map((x) => Category.fromJson(x))));
+    }
+    if (prefs.getString('booksByCategory') != null) {
+      _booksByCategory = Future.value(List<Future<List<Book>>>.from(
+          jsonDecode(prefs.getString('booksByCategory')!).map((x) =>
+              Future.value(List<Book>.from(x.map((x) => Book.fromJson(x)))))));
+    } else {
+      // Add a list for each category
+      // from the API
+      _categories.then((value) {
+        _booksByCategory = Future.value(List.generate(value.length, (index) {
+          return ApiService.getBooksByCategory(index + 1);
+        }));
+      });
+      // Save the list of lists to shared preferences
+      Future.delayed(const Duration(seconds: 5), () async {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        List<Future<List<Book>>> booksByCategoryFuture = await _booksByCategory;
+        List<List<Book>> booksByCategory =
+            await Future.wait(booksByCategoryFuture);
+        String booksByCategoryJson = jsonEncode(booksByCategory);
+        prefs.setString('booksByCategory', booksByCategoryJson);
+      });
     }
     _categories.then((value) => _length = value.length);
   }
@@ -47,6 +72,36 @@ class _CatalogState extends State<Catalog> {
   void saveBooks(List<Book> books) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString('books', jsonEncode(books));
+  }
+
+  void checkForNewBooks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<Book> currentBooks = [];
+    if (prefs.getString('books') != null) {
+      currentBooks = List<Book>.from(
+          jsonDecode(prefs.getString('books')!).map((x) => Book.fromJson(x)));
+    } else {
+      return;
+    }
+    int lastBookId = currentBooks.last.id;
+    bool newBooks = true;
+
+    while (newBooks) {
+      lastBookId++;
+      try {
+        Book book = await ApiService.getBook(lastBookId);
+        setState(() {
+          _books.then((value) => value.add(book));
+          for (var category in book.categories) {
+            _booksByCategory.then((value) {
+              value[category.id - 1].then((value) => value.add(book));
+            });
+          }
+        });
+      } catch (e) {
+        newBooks = false;
+      }
+    }
   }
 
   @override
@@ -140,7 +195,7 @@ class _CatalogState extends State<Catalog> {
                     // from the API
                     ...List.generate(_length, (index) {
                       return FutureBuilder<List<Book>>(
-                        future: ApiService.getBooksByCategory(index + 1),
+                        future: _booksByCategory.then((value) => value[index]),
                         builder: (context, snapshot) {
                           if (snapshot.hasData) {
                             List<Book> books = snapshot.data!;
